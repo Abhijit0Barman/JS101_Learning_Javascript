aead2d15d326022982fec781eee3ba3f975ced28
Abhijit0Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2023-01-30 17:42:07 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,3 +3,6 @@ im trying to learn githiub commands like
 name & email chamge=> git config --global --edit
 for exit=> :wq
 cheack status=> git status
+stage area=> git add .   => git add sum.java
+stage area then commit=> git commit -m demodemo  => git commit -m "initial comment"  
+cheack all commit=> git log
